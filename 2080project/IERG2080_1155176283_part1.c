eeb9cf2b2d2a0525cca3361d8d8c9344d8e8b7ff
Mariouz <joseph02252003@gmail.com>
2022-12-18 23:36:23 +0800

    sth

--- 2080project/IERG2080_1155176283_part1.c
+++ 2080project/IERG2080_1155176283_part1.c
@@ -29,20 +29,20 @@ int transfer_size;
 
 typedef struct Card{
 	enum suit color;//red green blue yellow wild empty;
-	char size;
+	int size;
 	struct Card* next;
 }Card;
 
 Card* Create_dummy_card(){
-	Card* tmp;
+	Card* tmp = (Card*)malloc(sizeof(tmp));
 	tmp->color=empty;
-	tmp->size='-';
+	tmp->size=0;
 	tmp->next=tmp;
 	return tmp;
 }
 
 Card* DrawCard(Card* player_deck , char color , int size){
-	Card* tmp;
+	Card* tmp = (Card*)malloc(sizeof(tmp));
 	tmp->size=size;
 	if(color=='R')tmp->color=red;
 	else if(color=='G')tmp->color=green;
@@ -111,16 +111,18 @@ int have_card(Card* player_deck , char color , int size){
 	return 0;
 }
 
+
 int main(int argc, char **argv){
 	int number_of_player=0;
 	int game_continue=1;
 	Card** player_deck = (Card**)malloc(sizeof(Card)*120);
 	player_deck[0]=Create_dummy_card();
 	player_deck[1]=Create_dummy_card();
+	
+
 	FILE* original_hand=fopen(argv[1] , "r");
-	if(!original_hand){
-		return 0;
-	}
+	if(!original_hand) return 0;
+
 	fscanf(original_hand , "%d" , &number_of_player);//assume one only
 	int player_no_;
 	fscanf(original_hand , "%d" , &player_no_);//assume one only
@@ -130,21 +132,36 @@ int main(int argc, char **argv){
 		player_deck[1]=DrawCard(player_deck[1] , c , d);
 	}
 	fclose(original_hand);
+
+
 	FILE* Carddeck = fopen(argv[2] , "r");
 	if(!Carddeck){
 		return 0;
 	}
-	while(fscanf(Carddeck , "%c%d" , &c , &d)!=EOF){
+
+	while(fscanf(Carddeck , "%c%d " , &c , &d)!=EOF){
 		player_deck[0]=DrawCard(player_deck[0] , c , d);
 	}
 	fclose(Carddeck);
-	FILE* table=fopen(argv[3] , "a");
+
+
+	FILE* table=fopen(argv[3] , "r");
 	if(!table){
 		return 0;
 	}
+
 	int tmp;
-	fscanf(table , "%d %c%d" , &tmp , &c , &d);
+	fscanf(table , "%d %c%d " , &tmp , &c , &d);
+
+	fclose(table);
+
+	table=fopen(argv[3] , "a");
+	if(!table){
+		return 0;
+	}
+
 	fprintf(table , "1 R1\n");
+
 	while(player_deck[0]->next!=player_deck[0]){
 		if(player_deck[0]->color==red){
 			fprintf(table , "1 R%d\n" , player_deck[0]->size);
@@ -162,8 +179,11 @@ int main(int argc, char **argv){
 	}
 	
 	fclose(table);
+
+
 	FILE* output=fopen("output.txt" , "w");
 	fprintf(output , "WIN:1\n1 NULL\n");
+	fclose(output);
 	return 0;
 }
 
